d37d80c7a00ee410c592f9385fbbda99bee73e8b
MaksOganesyan <101575384+MaksOganesyan@users.noreply.github.com>
2024-01-23 16:28:17 +0300

    Add files via upload

---
+++ learning_logs/urls.py
@@ -0,0 +1,20 @@
+from django.urls import path, include
+from . import views
+from django.contrib.auth import views as auth_views
+
+
+app_name = 'learning_logs'
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('topics/', views.topics, name='topics'),
+    path('topics/<int:topic_id>/', views.topic, name='topic'),
+    path('new_topic/', views.new_topic, name='new_topic'),
+    path('new_entry/<int:topic_id>/', views.new_entry, name='new_entry'),
+    path('edit_entry/<int:entry_id>/', views.edit_entry, name='edit_entry'),
+    path('users/', include(('users.urls', 'users'), namespace='users')),
+    path('accounts/login/', auth_views.LoginView.as_view(), name='login'),
+    path('accounts/logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('no_topics/', views.no_topics, name='no_topics'),
+
+]
